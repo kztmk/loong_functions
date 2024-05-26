30451fbcd86478525d35906f0471b51017974ec3
kztmk <kazuya@tmksolutions.com>
2024-05-26 11:34:17 -0700

    Fix: add error handling for firebase auth error

--- functions/src/loong/v1/auth/register.ts
+++ functions/src/loong/v1/auth/register.ts
@@ -1,7 +1,7 @@
-import * as admin from "firebase-admin";
-import * as functions from "firebase-functions";
-import {log} from "firebase-functions/logger";
-import {sendMailRegisterUser} from "../../../helper";
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import { log } from 'firebase-functions/logger';
+import { sendMailRegisterUser } from '../../../helper';
 
 // ユーザーから
 // - 購入時のメールアドレス
@@ -11,68 +11,76 @@ import {sendMailRegisterUser} from "../../../helper";
 // ユーザーが存在しない場合は, ユーザーを作成し, データベースに購入時のメールアドレスを保存する。
 // ユーザーが存在する場合は, その旨を返す。
 
-import {db} from "../../../index";
+import { db } from '../../../index';
 
 export const registerUser = functions.https.onRequest(async (req, res) => {
-  if (req.method !== "POST") {
-    res.status(405).send("Method Not Allowed");
+  if (req.method !== 'POST') {
+    res.status(405).send('Method Not Allowed');
     return;
   }
   // リクエストボディの存在を確認
   if (!req.body) {
-    res.status(400).send("Request body is missing");
+    res.status(400).send('Request body is missing');
     return;
   }
 
   // パラメータの取得
-  const {purchaseEmail, accountEmail, password} = req.body;
+  const { purchaseEmail, accountEmail, password } = req.body;
 
   // purchaseEmailの存在を確認
   if (!purchaseEmail) {
-    res.status(400).send("purchaseEmail is missing");
+    res.status(400).send('購入者用メールアドレスは必須です。');
     return;
   }
 
   // accountEmailの存在を確認
   if (!accountEmail) {
-    res.status(400).send("accountEmail is missing");
+    res.status(400).send('ID用メールアドレスは必須です。');
     return;
   }
 
   // passwordの存在を確認
   if (!password) {
-    res.status(400).send("password is missing");
+    res.status(400).send('パスワードは必須です。');
     return;
   }
 
-  const usersRef = db.collection("users");
+  const usersRef = db.collection('users');
   const snapshot = await usersRef
-    .where("purchaseEmail", "==", purchaseEmail)
+    .where('purchaseEmail', '==', purchaseEmail)
     .get();
 
-  log("create user request", purchaseEmail);
+  log('create user request', purchaseEmail);
 
   if (!snapshot.empty) {
-    log("user exists");
+    log('user exists');
     await sendMailRegisterUser(purchaseEmail, accountEmail, true);
-    res.status(200).send("User already exists");
+    res
+      .status(200)
+      .send('購入者用メールアドレスでアカウントが作成されています。');
   } else {
-    log("Create user: start");
+    log('Create user: start');
     const userRecord = await admin.auth().createUser({
       email: accountEmail,
       password: password,
     });
 
-    log("Successfully created new user:", userRecord.uid);
+    log('Successfully created new user:', userRecord.uid);
     // ユーザーが作成された後、データベースに購入時のメールアドレスを保存
-    await usersRef.doc(userRecord.uid).set({
-      purchaseEmail: purchaseEmail,
-      userId: userRecord.uid,
-      accountEmail: accountEmail,
-      timeStamp: admin.firestore.FieldValue.serverTimestamp(),
-    });
+    try {
+      await usersRef.doc(userRecord.uid).set({
+        purchaseEmail: purchaseEmail,
+        userId: userRecord.uid,
+        accountEmail: accountEmail,
+        timeStamp: admin.firestore.FieldValue.serverTimestamp(),
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .send('ID用メールアドレスのアカウントが既に存在します。');
+    }
 
-    log("Create user: end");
+    log('Create user: end');
     await sendMailRegisterUser(purchaseEmail, accountEmail, false);
     res.status(200).send(`User created: ${userRecord.uid}`);
   }
